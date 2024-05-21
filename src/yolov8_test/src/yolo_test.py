4a22c04793e66991531c8ab5dc1442c3578a7e01
orangeOAO <k98008@gmail.com>
2024-05-21 14:51:09 +0800

    test yolo

---
+++ src/yolov8_test/src/yolo_test.py
@@ -0,0 +1,19 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8n.pt")  # pretrained YOLOv8n model
+
+# Run batched inference on a list of images
+results = model(["bus.jpg"])  # return a list of Results objects
+
+# Process results list
+for result in results:
+    boxes = result.boxes  # Boxes object for bounding box outputs
+    masks = result.masks  # Masks object for segmentation masks outputs
+    keypoints = result.keypoints  # Keypoints object for pose outputs
+    probs = result.probs  # Probs object for classification outputs
+    obb = result.obb  # Oriented boxes object for OBB outputs
+    print("OAO")
+    print(obb)
+    result.show()  # display to screen
+    result.save(filename="result.jpg")  # save to disk
